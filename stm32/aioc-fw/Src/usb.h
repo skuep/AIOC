7e7c8af252c7de1ef357d26ad9d2c8cd13e0ace1
Simon Kueppers <simon.kueppers@skueppers.de>
2023-03-11 10:04:51 +0100

    Added automatic USB-reset when coming out of non-POR reset and added a handler for rebooting into the built-in DFU bootloader

--- stm32/aioc-fw/Src/usb.h
+++ stm32/aioc-fw/Src/usb.h
@@ -3,11 +3,14 @@
 
 #include "usb_audio.h"
 #include "usb_serial.h"
+#include "usb_dfu.h"
 
+#define USB_RESET_DELAY     100 /* milliseconds */
 #define USB_SOF_TIMER       TIM2
 #define USB_SOF_TIMER_CNT   TIM2->CNT
 #define USB_SOF_TIMER_HZ    72000000UL
 
+void USB_Reset(void);
 void USB_Init(void);
 void USB_Task(void);
 
