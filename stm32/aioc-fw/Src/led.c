95c850d09c06408fdee4a3e848d4c8d99b0d22ca
Simon Kueppers <simon.kueppers@skueppers.de>
2025-09-25 17:34:51 +0200

    Increase LED PWM frequency to 16kHz

--- stm32/aioc-fw/Src/led.c
+++ stm32/aioc-fw/Src/led.c
@@ -9,7 +9,7 @@ uint8_t LedLevels[2] = {LED_IDLE_LEVEL, LED_IDLE_LEVEL};
 uint16_t LedCounter = 0;
 uint16_t LedCounterPrev = 0;
 
-#define LED_SET1(x) LED_TIMER->CCR3 = (512 - ( ((uint32_t) (x)) & 0xFF ))
+#define LED_SET1(x) LED_TIMER->CCR3 = (500 - ( ((uint32_t) (x)) & 0xFF ))
 #define LED_SET2(x) LED_TIMER->CCR4 = ( ((uint32_t) (x)) & 0xFF )
 
 void LED_TIMER_IRQ(void)
@@ -64,7 +64,7 @@ void LED_TIMER_IRQ(void)
     }
 
     /* LEDs are connected anti-parallel */
-    LED_TIMER->CCR3 = (512 - (uint32_t) LedLevels[0]);
+    LED_TIMER->CCR3 = (500 - (uint32_t) LedLevels[0]);
     LED_TIMER->CCR4 = ( (uint32_t) LedLevels[1]);
 
     /* Advance counters */
@@ -91,12 +91,12 @@ void LED_Init(void)
     __HAL_RCC_TIM4_CLK_ENABLE();
 
     LED_TIMER->CR1 = TIM_CLOCKDIVISION_DIV1 | TIM_COUNTERMODE_UP | TIM_AUTORELOAD_PRELOAD_ENABLE;
-    LED_TIMER->ARR = 511;
-    LED_TIMER->PSC = (2 * HAL_RCC_GetPCLK1Freq() / 500000) - 1;
+    LED_TIMER->ARR = 500-1;
+    LED_TIMER->PSC = (2 * HAL_RCC_GetPCLK1Freq() / 8000000) - 1;
     LED_TIMER->CCMR2 =  TIM_OCMODE_PWM1 << 8 | TIM_OCMODE_PWM1 | TIM_CCMR2_OC3PE | TIM_CCMR2_OC4PE;
     LED_TIMER->CCER = (0 << TIM_CCER_CC3P_Pos) | (1 << TIM_CCER_CC4P_Pos) | TIM_CCER_CC3E | TIM_CCER_CC4E;
     LED_TIMER->CCR3 = 0;
-    LED_TIMER->CCR4 = 512;
+    LED_TIMER->CCR4 = 500;
     LED_TIMER->EGR = TIM_EGR_UG;
     LED_TIMER->DIER = TIM_DIER_UIE;
     LED_TIMER->CR1 |= TIM_CR1_CEN;
